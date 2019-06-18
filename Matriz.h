992f1cb4aaf98a25a232c10fe6074d80acab8103
sorem <sorem@sorem-Inspiron-3576>
2019-06-17 23:55:06 -0600

    - Se simplificó la matriz, ahora solo tiene un pivote inicial - Se modifició la forma de añadir nodos. Ahora se añaden y reemplazan nodos correctamente - Se añadió el método para generar el gráfico de nodos

--- Matriz.h
+++ Matriz.h
@@ -24,218 +24,284 @@ class Matriz{
     public:
         ///Constructor por defecto
         Matriz();
-        void AgregarEn(int fila, int col, string dato);
+        void AgregarEn(int fila,int col, string dato);
         int Filas() {return filas;}
         int Cols() {return cols;}
         string Dot();
         string Renderizar();
     private:
-        NodoM<string> *pivoteFilaInicio;
-        NodoM<string> *pivoteFilaFinal;
-        NodoM<string> *pivoteColInicio;
-        NodoM<string> *pivoteColFinal;
+        NodoM<string> *pivoteMatriz;
+        string PalDotRow(NodoM<string> *nodito);
+        string PalDotCol(NodoM<string> *nodito);
         int filas;
         int cols;
         bool esVacia;
+        //Sirve para saber si ya existe la esquina 0,0
+        bool YaExiste;
 };
 /**
  * Inicializa todas los parametros de la matriz
  */
 Matriz::Matriz(){
-    filas = 1;
-    cols = 1;
-    
-    pivoteFilaInicio = new NodoM<string>(true);
-    pivoteFilaInicio->Dato("");
-    
-    pivoteFilaFinal = new NodoM<string>(true);
-    pivoteFilaFinal->Dato("");
-    
-    pivoteColInicio = new NodoM<string>(true);
-    pivoteColInicio->Dato("");
-    
-    pivoteColFinal = new NodoM<string>(true);
-    pivoteColFinal->Dato("");
-    
-    pivoteFilaInicio->fila = 0;
-    pivoteColInicio->col = 0;
-    
-    pivoteFilaInicio->abajo = pivoteFilaFinal;
-    pivoteFilaFinal->arriba = pivoteFilaInicio;
-    pivoteFilaFinal = pivoteFilaInicio;
-    
-    pivoteColInicio->siguiente = pivoteColFinal;
-    pivoteColFinal->anterior = pivoteColInicio;
-    pivoteColFinal = pivoteFilaInicio;
-    
+    filas = 0;
+    cols = 0;    
     esVacia = true;
+    pivoteMatriz = new NodoM<string>(false);
+    pivoteMatriz->fila = -1;
+    pivoteMatriz->col = -1;
+    pivoteMatriz->Dato("Matriz");
+    YaExiste = false;
 }
-
 /**
  * Inserta el dato de un nodo de la matriz en la posición (fila, col) específicada
  * @param fila 
  * @param col
  * @param dato
  */
-void Matriz::AgregarEn(int fila, int col, string dato){
-    //Recorrer las n filas y luego las ncol
-    //Si no existe las crea en el instanste y las enlaza al nodofila/nodocolumna anterior inmediato
-    //Coloca el nodo en la posición especificada
-    NodoM<string> *filaPiv = pivoteFilaInicio;
-    NodoM<string> *colPiv = pivoteColInicio;
-    //Crea las filas necesarias
-    if(fila > (filas-1)){
-        //Crear las 'fila' filas necesarias
+void Matriz::AgregarEn(int fila,int col, string dato){
+    //Establece cuatro apuntadores para recorrer y agregar encabezados de filas o columnas si fuera necesarios
+    //y para recorrer y agregar el nodo (o modificarlo)
+    NodoM<string> *filaPiv = pivoteMatriz;
+    NodoM<string> *colPiv = pivoteMatriz;
+    NodoM<string> *filaAdd = pivoteMatriz;
+    NodoM<string> *colAdd = pivoteMatriz;
+    //Determina si exsite las filas necesarias
+    if (fila > (filas-1)){
+        //No hay filas suficientes
+        //Recorrer la fila hasta antes de llegar a un apuntador nulo
+        while(filaPiv->abajo){
+            //Recorre hacia abajo
+            filaPiv = filaPiv->abajo;
+        }
+        //Determina cuantas filas encabezado deberá agregar
         int difFila = fila - filas + 1;
-        int contadorFila = filas - 1 ;
+        //Agrega los nodos encabezados
         while(difFila){
             NodoM<string> *nuevoFila = new NodoM<string>(true);
-            //Establece el número de fila
-            nuevoFila->fila = contadorFila++;
-            //Enlaza la fila
-            nuevoFila->arriba = pivoteFilaFinal;
-            pivoteFilaFinal->abajo = nuevoFila;
-            pivoteFilaFinal = nuevoFila;
+            nuevoFila->fila = filaPiv->fila + 1;
+            //Enlaza los nodos
+            if (filaPiv->Dato().compare("Matriz")!=0)
+                nuevoFila->arriba = filaPiv;
+            filaPiv->abajo = nuevoFila;
             //Disminuye el contador
             difFila--;
+            //Aumenta las filas
             filas++;
+            //Mueve el apuntador
+            filaPiv = filaPiv->abajo;
         }
     }
-    //Crea las columnas necesarias
+    //Determina si existen las columnas necesarias
     if(col > (cols-1)){
+        //No hay cols suficientes
+        //Recorrer la col hasta antes de llegar a un apuntador nulo
+        while(colPiv->siguiente){
+            //recorre hacia siguiente;
+            colPiv = colPiv->siguiente;
+        }
+        //Determina cuántas columnas encabezado deberá agregar      
         //Crea las 'col' columnas necesarias
         int difCol = col - cols + 1;
-        int contadorCol = cols - 1;
         while(difCol){
             NodoM<string> *nuevoCol = new NodoM<string>(true);
-            //Establece el número de col
-            nuevoCol->col = contadorCol++;
-            //Enlaza la columna
-            nuevoCol->anterior = pivoteColFinal;
-            pivoteColFinal->siguiente = nuevoCol;
-            pivoteColFinal = nuevoCol;
+            nuevoCol->col = colPiv->col + 1;
+            //Enlaza los nodos
+            if (colPiv->Dato().compare("Matriz")!=0)
+                nuevoCol->anterior = colPiv;
+            colPiv->siguiente = nuevoCol;
             //Disminuye el contador
             difCol--;
+            //Aumenta las filas
             cols++;
+            //Mueve el apuntador
+            colPiv = colPiv->siguiente;
         }
     }
     //Recorre las filas
-    while(filaPiv->abajo!=NULL && filaPiv->abajo->fila <= fila){
+    while(filaAdd->abajo && filaAdd->abajo->fila <= fila){
         //Recorre hacia abajo
-        filaPiv = filaPiv->abajo;
+        filaAdd = filaAdd->abajo;
     }
     //Recorre las columnas
-    while(colPiv->siguiente!=NULL && colPiv->siguiente->col <= col){
+    while(colAdd->siguiente && colAdd->siguiente->col <= col){
         //Recorre hacia la derecha
-        colPiv = colPiv->siguiente;
+        colAdd = colAdd->siguiente;
     }
     //Se está en la coordenada deseada
     //Recorre hasta encontrar el nodo col o uno anterior a él 
-    while(filaPiv->siguiente != NULL && filaPiv->siguiente->col <= col){
-        filaPiv = filaPiv->siguiente;
+    while(filaAdd->siguiente && filaAdd->siguiente->col <= col){
+        filaAdd = filaAdd->siguiente;
     }
     //Recorre hasta encontrar el nodo fila o uno anterior a él
-    while(colPiv->abajo != NULL && colPiv->abajo->fila <= fila){
-        colPiv = colPiv->abajo;
+    while(colAdd->abajo && colAdd->abajo->fila <= fila){
+        colAdd = colAdd->abajo;
     }
     //Determina si la coordenada es igual a (fila, col)
-    if(colPiv->col == col && filaPiv->fila == fila)
+    /*bool a = colAdd->col == col;
+    bool b = colAdd->fila == fila;
+    bool c = filaAdd->fila == fila;
+    bool d = filaAdd->col == col;
+    cout << a << endl << b << endl << c << endl << d << endl;*/
+    if(colAdd->col == col && colAdd->fila == fila && filaAdd->fila == fila && filaAdd->col == col )
     {
-        //Solo cambia el dato de la coordenada
-        filaPiv->Dato(dato);
-        colPiv->Dato(dato);
-        esVacia = false;
+        //Es el encabezado, para el caso(0,0)
+        if (fila == 0 && col == 0){ 
+            if(YaExiste){
+                //Ya existe solo cambia el nodo siguiente
+                filaAdd->Dato(dato);
+                colAdd->Dato(dato);
+                return;
+            }
+            NodoM<string> *nuevo = new NodoM<string>(fila, col, dato);
+            //Enlaza el nodo nuevo a la filaPiv
+            nuevo->siguiente = filaAdd->siguiente;
+            nuevo->anterior = filaAdd;
+            if (filaAdd->siguiente)
+                filaAdd->siguiente->anterior = nuevo;
+            filaAdd->siguiente = nuevo;
+            //Enlaza el nodo nuevo a la colPiv
+            nuevo->arriba = colAdd;
+            nuevo->abajo = colAdd->abajo;
+            if(colAdd->abajo)
+                colAdd->abajo->arriba = nuevo;
+            colAdd->abajo = nuevo;
+            esVacia = false;
+            YaExiste = true;
+        } else {
+            filaAdd->Dato(dato);
+            colAdd->Dato(dato);
+        }
     } 
     else 
     {
         NodoM<string> *nuevo = new NodoM<string>(fila, col, dato);
         //Enlaza el nodo nuevo a la filaPiv
-        nuevo->siguiente = filaPiv->siguiente;
-        nuevo->anterior = filaPiv;
-        if (filaPiv->siguiente !=NULL)
-            filaPiv->siguiente->anterior = nuevo;
-        filaPiv->siguiente = nuevo;
+        nuevo->siguiente = filaAdd->siguiente;
+        nuevo->anterior = filaAdd;
+        if (filaAdd->siguiente)
+            filaAdd->siguiente->anterior = nuevo;
+        filaAdd->siguiente = nuevo;
         //Enlaza el nodo nuevo a la colPiv
-        nuevo->arriba = colPiv;
-        nuevo->abajo = colPiv->abajo;
-        if(colPiv->abajo != NULL)
-            colPiv->abajo->arriba = nuevo;
-        colPiv->abajo = nuevo;
+        nuevo->arriba = colAdd;
+        nuevo->abajo = colAdd->abajo;
+        if(colAdd->abajo)
+            colAdd->abajo->arriba = nuevo;
+        colAdd->abajo = nuevo;
         esVacia = false;
     }
+    //delete filaAdd, colAdd, filaPiv, colPiv;
 }
 /**
  * Genera el string que se utlizará para graficar la matriz
  * @return 
  */
 string Matriz::Dot(){
-    stringstream str;
-    if (esVacia) 
-        return str.str();
-    stringstream rangoAux;
-    str << "digraph sparce_matrix {" << endl;
-    str << "node [shape = box]" << endl;
-    str << "mtr [label =\"Matriz\"];" << endl;
-    //Enlaza toda las fila de encabezados columna con mtr
-    str << "mtr -> p" << pivoteColInicio << endl;
-    NodoM<string> *tempCol = pivoteColInicio;
-    while(tempCol != NULL){
-        str << "p" <<  tempCol << "[label =\""<< tempCol->col <<"\"]" <<endl;
-        rangoAux << "; p" << tempCol;
-        if (tempCol->anterior!=NULL)
-            str << "p" << tempCol->anterior << "-> p" << tempCol << endl;
-        if (tempCol->siguiente!=NULL)
-            str << "p" << tempCol->siguiente << "-> p" << tempCol << endl;
-        if (tempCol->abajo != NULL)
-            str << "p" << tempCol->abajo << "-> p" << tempCol << endl;
-        tempCol = tempCol->siguiente;
-    }
-    //Pone en el mismo rango a los encabezados de las columnas
-    str << endl << "{rank = same; mtr" << rangoAux.str() << "}" << endl;
-    //Recorrer las filas y columnas enlazando los nodos
-    //Coloca en el mismo rango a todas las filas
-    str << "mtr -> p" << pivoteFilaInicio << endl;
-    //Se utiliza para recorrer filas
-    NodoM<string> *tempFila = pivoteFilaInicio;
-    //Se utiliza para recorrer entre columnas
+    //Si está vacía no devuelve nada
+    if(esVacia)
+        return string();
+    stringstream retorno;
+    stringstream auxNodos;
+    stringstream auxFila;
+    stringstream auxCol;
+    NodoM<string> *tempFila_ = pivoteMatriz;
     NodoM<string> *pivFila;
-    //Recorre cada fila
-    while(tempFila != NULL){
-        //Asigna el nuevo nodo fila
-        pivFila = tempFila;
-        //Limpia el rangoAux
-        rangoAux.str( std::string() );
-        rangoAux.clear();
-        //Recorrer cada columna de la fila
-        while (pivFila != NULL){
-            str << "p" << pivFila << "[label =\"" ;
+    //Sive para priorizar las columna de las filas
+    bool flag = true;
+    //Siver para priorizar el grupo 1 en el nodo Matriz
+    bool flag1 = true;
+    bool flag2 = true;
+    //Recorre por primera vez toda la matriz creando todos los nodos
+    while(tempFila_){
+        pivFila = tempFila_;
+        //Recorrer las celdas
+        flag2 = true;
+        while(pivFila){
+            auxNodos << "p" << pivFila << "[label =\"" ;
             //Determina si es o no cabecera
-            if (pivFila->EsCabecera())
-                str << pivFila->fila;
-            else 
-                str << pivFila->Dato();
-            str << "\"]" << endl;
-            //Guarda el puntero para luego agregarlo al mismo rango 
-            rangoAux << "; p" << tempFila;
-            //Enlaza todos los nodos
-            if (pivFila->arriba != NULL)
-                str << "p" << pivFila->arriba << "-> p" << pivFila << endl;
-            if (pivFila->abajo != NULL)
-                str << "p" << pivFila->abajo << "-> p" << pivFila << endl;
-            if (pivFila->siguiente != NULL)
-                str << "p" << pivFila << "-> p" << pivFila->siguiente << " [constraint = false]" << endl;
-            if (pivFila->anterior != NULL)
-                str << "p" << pivFila->anterior << "-> p" << pivFila << " [constraint = false]" << endl;    
+            if (!pivFila->EsCabecera())
+                auxNodos << pivFila->Dato();
+            else
+                if (!flag){
+                    auxNodos << pivFila->fila;
+                } else {
+                    auxNodos << pivFila->col;
+                }
+            auxNodos << "\"; group = "; 
+            if (!flag1){
+                if (!flag2)
+                    auxNodos << pivFila->col + 2 ;
+                else {
+                    auxNodos << pivFila->col + 1;
+                    flag2 = false;
+                }
+            } else {
+                auxNodos << 1 ;
+                flag1 = false;
+            }
+            auxNodos << "]" << endl;
             //Recorre al siguiente nodo
             pivFila = pivFila->siguiente;
-        }        
-        //Establece todos los punteros de la fila al mismo rango
-        str << "{rank = same" << rangoAux.str() << "}" << endl;
+        }
         //Se mueve a la siguiente fila
+        tempFila_ = tempFila_->abajo;
+        flag = false;
+    }
+    //Recorre de nuevo la matriz creando las conexiones entre los nodos
+    //Recupera el "palindromo de la lista de encabezados columnas
+    string palDotRow = PalDotRow(pivoteMatriz->siguiente);
+    auxFila << "{rank=same; p"<< pivoteMatriz  << "->" << palDotRow.substr(0,palDotRow.length()-2) << ";}" << endl;
+    //A través de un ciclo recorre las filas y recupera su lista (conexiones)
+    NodoM<string> *tempFila = pivoteMatriz->abajo;
+    while(tempFila){
+        palDotRow = PalDotRow(tempFila);
+        auxFila << "{rank=same; " << palDotRow.substr(0, palDotRow.length()-2) << ";}" << endl;
         tempFila = tempFila->abajo;
     }
-    str << "}" << endl;
-    return str.str();
+    //Recorre por última vez la matriz creando las conexiones entre las columnas
+    //Recupera el palindromo de la lista de encabezados de fila
+    string palDotCol = PalDotCol(pivoteMatriz->abajo);
+    auxCol << "p" << pivoteMatriz << "->" <<palDotCol.substr(0,palDotCol.length()-2) << ";" << endl;
+    //A través de un ciclo recore la columnas y recupera su lista (conexiones)
+    NodoM<string> *tempCol = pivoteMatriz->siguiente;
+    while(tempCol){
+        palDotCol = PalDotCol(tempCol);
+        auxCol << palDotCol.substr(0,palDotCol.length()-2) << ";" << endl;
+        tempCol = tempCol->siguiente;
+    }
+    retorno << "digraph sparse_matrix {" << endl ;
+    retorno << "node [shape = box]" << endl;
+    retorno << auxNodos.str() << endl << auxFila.str() << endl <<auxCol.str() << endl;
+    retorno << "}";
+    return retorno.str();
 }
+/**
+ * Genera una cadena de nodos enlazados de forma "palindroma" desde una fila
+ * @param nodito
+ * @return 
+ */
+string Matriz::PalDotRow(NodoM<string> *nodito){
+    stringstream retorno;
+    retorno << "p" <<  nodito << "->";
+    if (nodito->siguiente != NULL) {
+        retorno << PalDotRow(nodito->siguiente);
+        retorno << "p" << nodito << "->";  
+    } 
+    return retorno.str();
+}
+/**
+ * Genera una cadena de nodos enlazados de forma "palindroma" desde una columna
+ * @param nodito
+ * @return 
+ */
+string Matriz::PalDotCol(NodoM<string> *nodito){
+    stringstream retorno;
+    retorno << "p" << nodito << "->";
+    if (nodito->abajo != NULL) {
+        retorno << PalDotCol(nodito->abajo);
+        retorno << "p" << nodito << "->";   
+    } 
+    return retorno.str();
+}
+
 #endif /* MATRIZ_H */
 
