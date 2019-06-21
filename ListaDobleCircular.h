66fa15404c8cecbaac99c9b93a90091121810b80
sorem <sorem@sorem-Inspiron-3576>
2019-06-21 13:52:26 -0600

    Se completaron las funciones de la lista circular doble para almcenar las ref de imagenes de los usuarios

--- ListaDobleCircular.h
+++ ListaDobleCircular.h
@@ -15,30 +15,62 @@
 #define LISTADOBLECIRCULAR_H
 #include "NodoL.h"
 #include "Imagen.h"
-
 #include <string>
 #include <iostream>
 #include <sstream>
 
 using namespace std;
 
-template <class T> class ListaDobleCircular{
+class ListaDobleCircular{
     public:
         ListaDobleCircular(): primero(0), ultimo(0), largo(0) {}
-        void AgregarAlFinal(Imagen dato);
+        void AgregarAlFinal(Imagen* dato);
+        void Eliminar(int id);
         int Largo(){return largo;}
-        string Dot(const string &nombre);
+        string Dot(const string &padre, int grupo);
     private:
-        NodoL<Imagen> *primero;
-        NodoL<Imagen> *ultimo;
+        
+        NodoL<Imagen*> *primero;
+        NodoL<Imagen*> *ultimo;
         int largo;
 };
+/**
+ * Elimina una imagen seǵun el id
+ * @param id
+ */
+void ListaDobleCircular::Eliminar(int id){
+    NodoL<Imagen*> *temp = primero;
+    if(temp->Dato()->Id() == id){
+        if(largo > 1){
+            inicio = temp->siguiente;
+            temp->siguiente->anterior =NULL;
+        }else {
+            inicio =NULL;
+        }
+        largo--;
+        delete temp;
+        return;
+    }
+    do{
+        if(temp->Dato()->Id() == id){
+            NodoL<Imagen*> *aux = temp->siguiente;
+            temp->anterior->siguiente = aux;
+            if (aux!=NULL)
+                aux->anterior = temp->anterior;
+            break;
+        }
+        temp = temp->siguiente;
+    }while(temp != primero);
+    largo--;
+    delete temp;
+    return;
+}
 /**
  * Agrega una nueva imagen al final de la lista
  * @param dato
  */
-template <class T> void ListaDobleCircular<T>::AgregarAlFinal(Imagen dato){
-    NodoL<Imagen> *nuevo = new NodoL<Imagen>(dato);
+void ListaDobleCircular::AgregarAlFinal(Imagen* dato){
+    NodoL<Imagen*> *nuevo = new NodoL<Imagen*>(dato);
     if(primero!=NULL){
         nuevo->siguiente = primero;
         nuevo->anterior = ultimo;
@@ -55,10 +87,22 @@ template <class T> void ListaDobleCircular<T>::AgregarAlFinal(Imagen dato){
  * @param nombre
  * @return 
  */
-template <class T> string ListaDobleCircular<T>::Dot(const string& nombre){
+string ListaDobleCircular::Dot(const string &padre, int grupo){
     stringstream str;
+    str << "subgraph cluster" << this << "{" << endl << "color=white" << endl;
+    str << padre << "->" << primero->Dato() << endl;
+    ///Servirá para recorrer la lista
+    NodoL<Imagen*> *temp = primero;
+    do {
+        str << "p" << temp->Dato() << "[label=\"Imagen]" <<  << "\"; group =" << grupo << "]" << endl;
+        if(temp->siguiente != primero){
+            NodoL<Imagen*> *aux = temp->siguiente;
+            str << "p" << temp->Dato() << "-> p" << aux->Dato() << endl;
+        }
+        temp = temp->siguiente;
+    } while (temp != primero);
+    str << "}" ;
     return str.str();
 }
-
 #endif /* LISTADOBLECIRCULAR_H */
 
