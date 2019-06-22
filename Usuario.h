2e4938a566889bb9921f342257a1498c90ce42e0
sorem <sorem@sorem-Inspiron-3576>
2019-06-21 19:15:49 -0600

    Se completó la clase Usuario

--- Usuario.h
+++ Usuario.h
@@ -13,14 +13,23 @@
 
 #ifndef USUARIO_H
 #define USUARIO_H
+#include <string>
+#include <sstream>
+#include <iostream>
+#include "ListaDobleCircular.h"
+#include "Imagen.h"
+
+using namespace std;
 
 class Usuario {
 public:
-    Usuario();
-    Usuario(const Usuario& orig);
-    virtual ~Usuario();
+    Usuario(string id_, ListaDobleCircular *lista_): id(id_) , lista(lista_){} 
+    ListaDobleCircular *Imagenes() {return lista;}
+    string Id() {return id;}
 private:
-
+    string id;
+    //Lista con enteros que especifican id de imagenes
+    ListaDobleCircular *lista;
 };
 
 #endif /* USUARIO_H */
